48ae54bcda0d55a7a4e1c0a588c45aca1758b2ab
Mitchell Lee <mitchell@Users-MacBook.local>
2016-09-10 13:51:21 -0400

    s

--- terminal.js
+++ terminal.js
@@ -108,7 +108,7 @@ function about(){
       setTimeout(function(){
       $("#psuedo-input").val("")
       $("#command").empty()
-      $("#text").empty().append("<p> My name is Mitchell Lee and I am an aspiring web developer. I am a Southern California native currently studying Computer Information Technology at Purdue University. I like all things technology whether it be hardware or software, and I am excited to see how technology will change the world for my generation </p>" + "<p>Some of My Hobbies:</p>" + "<ul>" +"<li>Hackathons</li>" +"<li>Airsoft</li>" +"<li>Anime</li>"+"<li>pondering about the future</li>"+"</ul>").show()
+      $("#text").empty().append("<p>My name is Mitchell Lee and I am an aspiring web developer. I am a Southern California native currently studying Computer Information Technology at Purdue University. I like all things technology whether it be hardware or software, and I am excited to see how technology will change the world for my generation </p>" + "<p>Some of My Hobbies:</p>" + "<ul>" +"<li>Hackathons</li>" +"<li>Airsoft</li>" +"<li>Anime</li>"+"<li>pondering about the future</li>"+"</ul>").show()
       $("html, body").scrollTop( $(document).height() );
     }, 1200)
     }
